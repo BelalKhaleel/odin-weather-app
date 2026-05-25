d4becccc97889ae041d6d9ee9dc58574ee5e79b7
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2026-05-25 11:45:36 +0300

    Use es6 syntax instead of common js for webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.cjs');
+ import { merge } from 'webpack-merge';
+ import common from './webpack.common.js';
 
-module.exports = merge(common, {
+ export default merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
